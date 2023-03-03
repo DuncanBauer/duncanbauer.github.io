e41aed5bdbc65caa56c7ce67f9308d97041edbca
Duncan <duncanebauer@gmail.com>
2023-03-02 19:36:58 -0800

    ....

--- src/components/molecules/Project.js
+++ src/components/molecules/Project.js
@@ -6,7 +6,8 @@ export default function Project(props) {
             <br/>
             <br/>
             <br/>
-            <label className={props.style_1}>{props.name}</label>
+            {/* <label className={props.style_1}>{props.name}</label> */}
+            <a href="http://duncanbauer.github.io/ProjectAurora/" className={props.style_1}>{props.name}</a>
             <p className={props.style_2}>{props.description}</p>
         </div>
     )
